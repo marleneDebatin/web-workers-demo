dec45e6ba10afd2b2e3b91e0b1f73c4eaf3bcdc0
Pia Droop <pia.droop@gmail.com>
2016-04-04 18:34:36 +0200

    finish web worker
    this should be a working web worker, but it can't be tested without a
    server

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -42,21 +42,38 @@
 
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
+    var worker = new Worker("/scripts/worker.js");
+    var message = {
+          "imageData" : imageData,
+          "type" : type
+   };
+    worker.postMessage(message);
 
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
-    }
-    toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
+    // length = imageData.data.length / 4;
+    // for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
+    //   r = imageData.data[i * 4 + 0];
+    //   g = imageData.data[i * 4 + 1];
+    //   b = imageData.data[i * 4 + 2];
+    //   a = imageData.data[i * 4 + 3];
+    //   pixel = manipulate(type, r, g, b, a);
+    //   imageData.data[i * 4 + 0] = pixel[0];
+    //   imageData.data[i * 4 + 1] = pixel[1];
+    //   imageData.data[i * 4 + 2] = pixel[2];
+    //   imageData.data[i * 4 + 3] = pixel[3];
+    // }
+
+    worker.addEventListener("message", function(e) {
+          if(e.data == undefined) {
+                console.log("Received undefined from worker.");
+          }
+          else {
+                imageData = e.data;
+                console.log("Something was received!");
+                toggleButtonsAbledness();
+                return ctx.putImageData(imageData, 0, 0);
+          }
+   }, false);
+
   };
 
   function revertImage() {
